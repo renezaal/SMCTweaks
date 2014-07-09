4d200bfc0252b21d959f7416f4123cc491208f6d
ReneZaal <renezaal@gmail.com>
2014-07-09 10:19:53 +0200

    base mod project

--- src/main/java/nl/sahl/smctweaks/SMCTweaks.java
+++ src/main/java/nl/sahl/smctweaks/SMCTweaks.java
@@ -1,5 +1,31 @@
 package nl.sahl.smctweaks;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "smctweaks", name = "Sahl's Gameplay tweaks", version = "1.7.2-0.1")
 public class SMCTweaks
 {
+    @Mod.Instance("smctweaks")
+    public static SMCTweaks instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
